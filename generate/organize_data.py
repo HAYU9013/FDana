995c825afad73dc7d134e4d219cfcecbafd455f5
HAYU9013 <114861748+HAYU9013@users.noreply.github.com>
2025-03-09 18:21:57 +0800

    建立批次建立系統

--- generate/organize_data.py
+++ generate/organize_data.py
@@ -1,11 +1,11 @@
 import os
 import shutil
 
-def organize_hspace(base_dir):
+def organize_hspace(base_dir, folder_name):
 
     # 定義路徑
-    base_dir = base_dir + r'\hspace'
-    photo_dir = os.path.join(base_dir, 'photo')
+    base_dir = base_dir + r'\lab\hspace'
+    photo_dir = os.path.join(base_dir, folder_name)
     original_dir = os.path.join(photo_dir, 'original')
     addnoise_dir = os.path.join(photo_dir, 'addnoise')
     # 建立資料夾
@@ -27,10 +27,10 @@ def organize_hspace(base_dir):
             # 移動到 addnoise 資料夾
             shutil.move(file_path, os.path.join(addnoise_dir, file_name))
 
-def organize_latents(base_dir):
+def organize_latents(base_dir, folder_name):
     # 定義路徑
     base_dir = base_dir + r'\latents'
-    photo_dir = os.path.join(base_dir, 'photo')
+    photo_dir = os.path.join(base_dir,  folder_name)
     original_dir = os.path.join(photo_dir, 'original')
     addnoise_dir = os.path.join(photo_dir, 'addnoise')
 
@@ -47,9 +47,38 @@ def organize_latents(base_dir):
         elif 'addnoise' in file_name:
             shutil.move(file_path, os.path.join(addnoise_dir, file_name))
 
-def move_to_analyze(target_dir):
+def move_to_analyze(base_dir, folder_name, category, target_dir):
+    latent_dir = os.path.join(base_dir, 'latents', folder_name)
+    hspace_dir = os.path.join(base_dir,'lab', 'hspace', folder_name)
+    latent_analyze_dir = os.path.join(target_dir, 'latent', category)
+    hspace_analyze_dir = os.path.join(target_dir, 'hspace', category)
 
-def organize_data():
+    os.makedirs(latent_analyze_dir, exist_ok=True)
+    os.makedirs(hspace_analyze_dir, exist_ok=True)
+
+    shutil.move(latent_dir, latent_analyze_dir)
+    shutil.move(hspace_dir, hspace_analyze_dir)
+
+    # input_dir = os.path.join(base_dir, '..', 'input')
+    # target_input_dir = os.path.join(latent_analyze_dir, '..', '..', 'input')
+
+    # print(input_dir)
+    # print(target_input_dir)
+    
+    # os.makedirs(target_input_dir, exist_ok=True)
+    
+    # for file_name in os.listdir(input_dir):
+    #     if folder_name+".png" == file_name:
+    #         shutil.copy(os.path.join(input_dir, file_name), os.path.join(target_input_dir, file_name))
+    
+
+
+def organize_data(folder_name = "photo", category = "car"):
     base_dir = r'C:\coding\Professer Proj\Comfy\newMyComfy\myComfyUI\output'
-    organize_hspace(base_dir)
-    organize_latents(base_dir)
+    target_dir = r'C:\coding\Professer Proj\Comfy\newMyComfy\FDanalyze\analyze'
+    organize_hspace(base_dir=base_dir, folder_name=folder_name)
+    organize_latents(base_dir=base_dir, folder_name=folder_name)
+    move_to_analyze(base_dir=base_dir, folder_name=folder_name, category=category, target_dir=target_dir)
+
+if __name__ == '__main__':
+    organize_data(folder_name="redcar", category="car")
